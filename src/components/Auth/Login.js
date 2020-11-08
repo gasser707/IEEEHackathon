06a23a00fe23c85cd51e023b58ae95dce1ba6c07
Gasser <gaserkhaled@yahoo.com>
2020-11-08 11:18:25 -0500

    m

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -62,7 +62,7 @@ class Login extends React.Component {
         <Grid.Column style={{ maxWidth: 450 }}>
           <Header as="h1" icon color="violet" textAlign="center">
             <Icon name="code branch" color="violet" />
-            Login to G-Slack
+            Login to KidsLearn
           </Header>
           <Form onSubmit={this.handleSubmit} size="large">
             <Segment stacked>
